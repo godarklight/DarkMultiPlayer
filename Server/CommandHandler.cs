96bda74511f04641ef287b803c268a99a720a9db
Xandaros <mz.bremerhaven@googlemail.com>
2014-05-12 22:58:47 +0200

    Crude command handler
    Handles commands, but lacks advanced features like a history.
    Adds two commands: exit and help

--- Server/CommandHandler.cs
+++ Server/CommandHandler.cs
@@ -1,15 +1,90 @@
 using System;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace DarkMultiPlayerServer
 {
     public class CommandHandler
     {
+        private static Dictionary<string, Command> commands = new Dictionary<string, Command>();
+
         public static void ThreadMain()
         {
+            //Init
+            CommandHandler.RegisterCommand("help", (x) => CommandHandler.DisplayHelp(), "Displays this help");
+
+            //Main loop
             while (Server.serverRunning)
             {
-                Thread.Sleep(10);
+                string input = Console.ReadLine();
+                string[] split = input.Split(new char[]{' '});
+                if (split.Length > 0)
+                {
+                    string command = split[0];
+                    if (commands.ContainsKey(command))
+                    {
+                        commands[command].func(split);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unknown command: " + command);
+                    }
+                }
+            }
+        }
+
+        public static void RegisterCommand(string command, Action<string[]> func, string description)
+        {
+            Command cmd = new Command(command, func, description);
+            commands.Add(command, cmd);
+        }
+
+        public static void RegisterCommand(string command, Action<string[]> func)
+        {
+            RegisterCommand(command, func, "");
+        }
+
+        private static void DisplayHelp()
+        {
+            List<Command> commands = new List<Command>();
+            int longestName = 0;
+            foreach (Command cmd in CommandHandler.commands.Values)
+            {
+                commands.Add(cmd);
+                if (cmd.name.Length > longestName)
+                    longestName = cmd.name.Length;
+            }
+            commands.Sort();
+            foreach (Command cmd in commands)
+            {
+                if (cmd.description != "")
+                {
+                    Console.WriteLine("{0,-" + (longestName) + "} - {1}", cmd.name, cmd.description);
+                }
+                else
+                {
+                    Console.WriteLine(cmd.name);
+                }
+            }
+        }
+
+        private class Command : IComparable
+        {
+            public string name;
+            public Action<string[]> func;
+            public string description;
+
+            public Command(string name, Action<string[]> func, string description)
+            {
+                this.name = name;
+                this.func = func;
+                this.description = description;
+            }
+
+            public int CompareTo(object obj)
+            {
+                var cmd = obj as Command;
+                return this.name.CompareTo(cmd.name);
             }
         }
     }
