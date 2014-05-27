1a00980d9ba4fa41e864c97d3365ca3cb34e0646
Christopher Andrews <godarklight@gmail.com>
2014-05-27 22:50:12 +1000

    Backup the token and settings file in the saves folder, and restore them if the player deletes the Data folder.

--- Client/Settings.cs
+++ Client/Settings.cs
@@ -19,7 +19,9 @@ namespace DarkMultiPlayer
         private const int DEFAULT_CACHE_SIZE = 100;
         private string dataLocation;
         private string settingsFile;
+        private string backupSettingsFile;
         private string tokenFile;
+        private string backupTokenFile;
 
         public static Settings fetch
         {
@@ -33,7 +35,9 @@ namespace DarkMultiPlayer
         {
             dataLocation = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Data");
             settingsFile = Path.Combine(dataLocation, SETTINGS_FILE);
+            backupSettingsFile = Path.Combine(Path.Combine(Path.Combine(KSPUtil.ApplicationRootPath, "saves"), "DarkMultiPlayer"), SETTINGS_FILE);
             tokenFile = Path.Combine(dataLocation, TOKEN_FILE);
+            backupTokenFile = Path.Combine(Path.Combine(Path.Combine(KSPUtil.ApplicationRootPath, "saves"), "DarkMultiPlayer"), TOKEN_FILE);
             LoadSettings();
         }
 
@@ -45,15 +49,26 @@ namespace DarkMultiPlayer
             {
                 bool saveXMLAfterLoad = false;
                 XmlDocument xmlDoc = new XmlDocument();
+                if (File.Exists(backupSettingsFile) && !File.Exists(settingsFile))
+                {
+                    DarkLog.Debug("Restoring player settings file!");
+                    File.Copy(backupSettingsFile, settingsFile);
+                }
                 if (!File.Exists(settingsFile))
                 {
                     xmlDoc.LoadXml(newXMLString());
                     playerName = DEFAULT_PLAYER_NAME;
                     xmlDoc.Save(settingsFile);
                 }
+                if (!File.Exists(backupSettingsFile))
+                {
+                    DarkLog.Debug("Backing up player token and settings file!");
+                    File.Copy(settingsFile, backupSettingsFile);
+                }
                 xmlDoc.Load(settingsFile);
                 playerName = xmlDoc.SelectSingleNode("/settings/global/@username").Value;
-                try {
+                try
+                {
                     cacheSize = Int32.Parse(xmlDoc.SelectSingleNode("/settings/global/@cache-size").Value);
                 }
                 catch
@@ -72,7 +87,8 @@ namespace DarkMultiPlayer
                     Int32.TryParse(xmlNode.Attributes["port"].Value, out newServer.port);
                     servers.Add(newServer);
                 }
-                if (saveXMLAfterLoad) {
+                if (saveXMLAfterLoad)
+                {
                     SaveSettings();
                 }
             }
@@ -84,6 +100,13 @@ namespace DarkMultiPlayer
             //Read player token
             try
             {
+                //Restore backup if needed
+                if (File.Exists(backupTokenFile) && !File.Exists(tokenFile))
+                {
+                    DarkLog.Debug("Restoring backed up token file!");
+                    File.Copy(backupTokenFile, tokenFile);
+                }
+                //Load or create token file
                 if (File.Exists(tokenFile))
                 {
                     using (StreamReader sr = new StreamReader(tokenFile))
@@ -100,6 +123,11 @@ namespace DarkMultiPlayer
                         sw.WriteLine(playerGuid.ToString());
                     }
                 }
+                //Save backup token file if needed
+                if (!File.Exists(backupTokenFile)) {
+                    DarkLog.Debug("Backing up token file.");
+                    File.Copy(tokenFile, backupTokenFile);
+                }
             }
             catch
             {
@@ -113,6 +141,8 @@ namespace DarkMultiPlayer
                 {
                     sw.WriteLine(playerGuid.ToString());
                 }
+                DarkLog.Debug("Backing up token file.");
+                File.Copy(tokenFile, backupTokenFile);
             }
         }
 
@@ -128,10 +158,12 @@ namespace DarkMultiPlayer
                 xmlDoc.LoadXml(newXMLString());
             }
             xmlDoc.SelectSingleNode("/settings/global/@username").Value = playerName;
-            try {
+            try
+            {
                 xmlDoc.SelectSingleNode("/settings/global/@cache-size").Value = cacheSize.ToString();
             }
-            catch {
+            catch
+            {
                 XmlAttribute cacheAttribute = xmlDoc.CreateAttribute("cache-size");
                 cacheAttribute.Value = DEFAULT_CACHE_SIZE.ToString();
                 xmlDoc.SelectSingleNode("/settings/global").Attributes.Append(cacheAttribute);
@@ -147,6 +179,7 @@ namespace DarkMultiPlayer
                 serverNodeList.AppendChild(serverElement);
             }
             xmlDoc.Save(settingsFile);
+            File.Copy(settingsFile, backupSettingsFile);
         }
 
         private string newXMLString()
