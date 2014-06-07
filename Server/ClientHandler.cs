ecd4b58cc4d0b8cebeb1d0d86d4645b5dc21cbbe
RockyTV <rockytvbr@gmail.com>
2014-06-07 11:48:05 -0300

    Replace \n with a newline in the motd.

--- Server/ClientHandler.cs
+++ Server/ClientHandler.cs
@@ -2432,6 +2432,7 @@ namespace DarkMultiPlayerServer
             string newMotd = Settings.settingsStore.serverMotd;
             newMotd = newMotd.Replace("%name%", client.playerName);
             newMotd = newMotd.Replace("%servername%", Settings.settingsStore.shortname);
+            newMotd = newMotd.Replace(@"\n", Environment.NewLine);
 
             using (MessageWriter mw = new MessageWriter())
             {
