3f8b95ac68f783397da4c672817d75c54d696f5a
Fernando Becklas <fbecklas@gmail.com>
2017-10-06 17:39:44 -0300

    Fix asteroid issues in 1.3
    * Detect scenario DiscoverableObjects changed name in 1.3
    
    * Shorten iterations over Vessel lists when counting asteroids
    
    * Compatibility with the new Sentinel mechanic in 1.3
    
    * Set new asteroids lifetime = infinity

--- Client/AsteroidWorker.cs
+++ Client/AsteroidWorker.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using SentinelMission;
 
 namespace DarkMultiPlayer
 {
@@ -11,8 +11,9 @@ namespace DarkMultiPlayer
         public bool workerEnabled;
         //private state variables
         private float lastAsteroidCheck;
-        private const float ASTEROID_CHECK_INTERVAL = 5f;
-        ScenarioDiscoverableObjects scenarioController;
+        private const float ASTEROID_CHECK_INTERVAL = 60f; // 1 minute
+        private ScenarioDiscoverableObjects scenario;
+        private bool initialized;
         private List<string> serverAsteroids = new List<string>();
         private Dictionary<string, string> serverAsteroidTrackStatus = new Dictionary<string, string>();
         private object serverAsteroidListLock = new object();
@@ -29,81 +30,98 @@ namespace DarkMultiPlayer
             this.networkWorker = networkWorker;
             this.vesselWorker = vesselWorker;
             this.dmpGame.updateEvent.Add(Update);
-            GameEvents.onGameSceneLoadRequested.Add(OnGameSceneLoadRequested);
             GameEvents.onVesselCreate.Add(OnVesselCreate);
+        }
+
+        public void InitializeScenario()
+        {
+            foreach (ProtoScenarioModule psm in HighLogic.CurrentGame.scenarios)
+            {
+                if (psm != null && scenario == null && psm.moduleName.Contains("Discoverable"))
+                {
+                    scenario = (ScenarioDiscoverableObjects)psm.moduleRef;  // this is borked as of 1.3.0; maybe they'll fix it in the future?
+                }
+            }
+            if (scenario != null) scenario.spawnInterval = float.MaxValue;
+
+            // Disable the new Sentinel mechanic in KSP 1.3.0
+            SentinelUtilities.SpawnChance = 0f;
+
+            initialized = true;
+        }
+
+        public IEnumerable<Vessel> SpawnAsteroids(int quantity = 1)
+        {
+            System.Random random = new System.Random();
+            while (quantity-- > 0)
+            {
+                yield return SpawnAsteroid(random).vesselRef;
+            }
+            yield break;
+        }
+
+        public ProtoVessel SpawnAsteroid(System.Random random)
+        {
+            double baseDays = 21 + random.NextDouble() * 360;
+            Orbit orbit = Orbit.CreateRandomOrbitFlyBy(Planetarium.fetch.Home, baseDays);
+
+            ProtoVessel pv = DiscoverableObjectsUtil.SpawnAsteroid(
+                DiscoverableObjectsUtil.GenerateAsteroidName(),
+                orbit,
+                (uint)SentinelUtilities.RandomRange(random),
+                SentinelUtilities.WeightedAsteroidClass(random),
+                double.PositiveInfinity, double.PositiveInfinity);
+            return pv;
         }
 
         private void Update()
         {
-            if (workerEnabled)
+            if (!workerEnabled) return;
+            if (Client.realtimeSinceStartup - lastAsteroidCheck < ASTEROID_CHECK_INTERVAL) return;
+                else lastAsteroidCheck = Client.realtimeSinceStartup;
+            if (!initialized) InitializeScenario();
+            
+            //Try to acquire the asteroid-spawning lock if nobody else has it.
+            if (!lockSystem.LockExists("asteroid-spawning"))
             {
-                if (scenarioController == null)
+                lockSystem.AcquireLock("asteroid-spawning", false);
+            }
+
+            //We have the spawn lock, lets do stuff.
+            if (lockSystem.LockIsOurs("asteroid-spawning"))
+            {
+                if ((HighLogic.CurrentGame.flightState.protoVessels != null) && (FlightGlobals.fetch.vessels != null))
                 {
-                    foreach (ProtoScenarioModule psm in HighLogic.CurrentGame.scenarios)
+                    if ((HighLogic.CurrentGame.flightState.protoVessels.Count == 0) || (FlightGlobals.fetch.vessels.Count > 0))
                     {
-                        if (psm != null)
+                        int beforeSpawn = GetAsteroidCount();
+                        int asteroidsToSpawn = maxNumberOfUntrackedAsteroids - beforeSpawn;
+                        if (asteroidsToSpawn > 0)
                         {
-                            if (psm.moduleName == "ScenarioDiscoverableObjects")
-                            {
-                                if (psm.moduleRef != null)
-                                {
-                                    scenarioController = (ScenarioDiscoverableObjects)psm.moduleRef;
-                                    scenarioController.spawnInterval = float.MaxValue;
-                                }
-                            }
+                            foreach (Vessel asty in SpawnAsteroids(1)) // spawn 1 every ASTEROID_CHECK_INTERVAL seconds
+                                DarkLog.Debug("Spawned asteroid " + asty.name + ", have " + (beforeSpawn) + ", need " + maxNumberOfUntrackedAsteroids);
                         }
                     }
                 }
+            }
 
-                if (scenarioController != null)
+            //Check for changes to tracking
+            foreach (Vessel asteroid in GetCurrentAsteroids())
+            {
+                if (asteroid.state != Vessel.State.DEAD)
                 {
-                    if ((Client.realtimeSinceStartup - lastAsteroidCheck) > ASTEROID_CHECK_INTERVAL)
+                    if (!serverAsteroidTrackStatus.ContainsKey(asteroid.id.ToString()))
                     {
-                        lastAsteroidCheck = Client.realtimeSinceStartup;
-                        //Try to acquire the asteroid-spawning lock if nobody else has it.
-                        if (!lockSystem.LockExists("asteroid-spawning"))
+                        serverAsteroidTrackStatus.Add(asteroid.id.ToString(), asteroid.DiscoveryInfo.trackingStatus.Value);
+                    }
+                    else
+                    {
+                        if (asteroid.DiscoveryInfo.trackingStatus.Value != serverAsteroidTrackStatus[asteroid.id.ToString()])
                         {
-                            lockSystem.AcquireLock("asteroid-spawning", false);
-                        }
-
-                        //We have the spawn lock, lets do stuff.
-                        if (lockSystem.LockIsOurs("asteroid-spawning"))
-                        {
-                            if ((HighLogic.CurrentGame.flightState.protoVessels != null) && (FlightGlobals.fetch.vessels != null))
-                            {
-                                if ((HighLogic.CurrentGame.flightState.protoVessels.Count == 0) || (FlightGlobals.fetch.vessels.Count > 0))
-                                {
-                                    int beforeSpawn = GetAsteroidCount();
-                                    int asteroidsToSpawn = maxNumberOfUntrackedAsteroids - beforeSpawn;
-                                    for (int asteroidsSpawned = 0; asteroidsSpawned < asteroidsToSpawn; asteroidsSpawned++)
-                                    {
-                                        DarkLog.Debug("Spawning asteroid, have " + (beforeSpawn + asteroidsSpawned) + ", need " + maxNumberOfUntrackedAsteroids);
-                                        scenarioController.SpawnAsteroid();
-                                    }
-                                }
-                            }
-                        }
-
-                        //Check for changes to tracking
-                        foreach (Vessel asteroid in GetCurrentAsteroids())
-                        {
-                            if (asteroid.state != Vessel.State.DEAD)
-                            {
-                                if (!serverAsteroidTrackStatus.ContainsKey(asteroid.id.ToString()))
-                                {
-                                    serverAsteroidTrackStatus.Add(asteroid.id.ToString(), asteroid.DiscoveryInfo.trackingStatus.Value);
-                                }
-                                else
-                                {
-                                    if (asteroid.DiscoveryInfo.trackingStatus.Value != serverAsteroidTrackStatus[asteroid.id.ToString()])
-                                    {
-                                        ProtoVessel pv = asteroid.BackupVessel();
-                                        DarkLog.Debug("Sending changed asteroid, new state: " + asteroid.DiscoveryInfo.trackingStatus.Value + "!");
-                                        serverAsteroidTrackStatus[asteroid.id.ToString()] = asteroid.DiscoveryInfo.trackingStatus.Value;
-                                        networkWorker.SendVesselProtoMessage(pv, false, false);
-                                    }
-                                }
-                            }
+                            ProtoVessel pv = asteroid.BackupVessel();
+                            DarkLog.Debug("Sending changed asteroid, new state: " + asteroid.DiscoveryInfo.trackingStatus.Value + "!");
+                            serverAsteroidTrackStatus[asteroid.id.ToString()] = asteroid.DiscoveryInfo.trackingStatus.Value;
+                            networkWorker.SendVesselProtoMessage(pv, false, false);
                         }
                     }
                 }
@@ -181,17 +199,15 @@ namespace DarkMultiPlayer
         /// <param name="checkVessel">The vessel to check</param>
         public bool VesselIsAsteroid(ProtoVessel checkVessel)
         {
-            if (checkVessel != null)
-            {
-                if (checkVessel.protoPartSnapshots != null ? (checkVessel.protoPartSnapshots.Count == 1) : false)
-                {
-                    if (checkVessel.protoPartSnapshots[0].partName == "PotatoRoid")
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
+            // Short circuit evaluation = faster
+            if (
+                checkVessel != null
+                && checkVessel.protoPartSnapshots != null
+                && checkVessel.protoPartSnapshots.Count == 1
+                && checkVessel.protoPartSnapshots[0].partName == "PotatoRoid")
+                return true;
+            else
+                return false;
         }
 
         private Vessel[] GetCurrentAsteroids()
@@ -209,25 +225,7 @@ namespace DarkMultiPlayer
 
         private int GetAsteroidCount()
         {
-            List<string> seenAsteroids = new List<string>();
-            foreach (Vessel checkAsteroid in GetCurrentAsteroids())
-            {
-                if (!seenAsteroids.Contains(checkAsteroid.id.ToString()))
-                {
-                    seenAsteroids.Add(checkAsteroid.id.ToString());
-                }
-            }
-            foreach (ProtoVessel checkAsteroid in HighLogic.CurrentGame.flightState.protoVessels)
-            {
-                if (VesselIsAsteroid(checkAsteroid))
-                {
-                    if (!seenAsteroids.Contains(checkAsteroid.vesselID.ToString()))
-                    {
-                        seenAsteroids.Add(checkAsteroid.vesselID.ToString());
-                    }
-                }
-            }
-            return seenAsteroids.Count;
+            return GetCurrentAsteroids().Length;
         }
 
         /// <summary>
@@ -250,16 +248,9 @@ namespace DarkMultiPlayer
             }
         }
 
-        private void OnGameSceneLoadRequested(GameScenes scene)
-        {
-            //Force the worker to find the scenario module again.
-            scenarioController = null;
-        }
-
         public void Stop()
         {
             dmpGame.updateEvent.Remove(Update);
-            GameEvents.onGameSceneLoadRequested.Remove(OnGameSceneLoadRequested);
             GameEvents.onVesselCreate.Remove(OnVesselCreate);
         }
     }
