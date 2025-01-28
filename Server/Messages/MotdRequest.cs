3743a15417a5a3b811d4008000285ca5bb062243
BraveCaperCat2 <113925022+BraveCaperCat2@users.noreply.github.com>
2025-01-28 09:51:27 +0000

    Update MotdRequest.cs
    Add %randombody% to the MOTD.

--- Server/Messages/MotdRequest.cs
+++ Server/Messages/MotdRequest.cs
@@ -17,9 +17,13 @@ namespace DarkMultiPlayerServer.Messages
             newMessage.type = ServerMessageType.MOTD_REPLY;
 
             Random random = new Random();
+
+            List<string> randomBodies = Settings.settingsStore.randomBodies;
+            string randomBody = randomBodies[random.Next(randomBodies.Count)];
             
             string newMotd = Settings.settingsStore.serverMotd;
             newMotd = newMotd.Replace("%name%", client.playerName);
+            newMotd = newMotd.Replace("%randombody%", randomBody);
             newMotd = newMotd.Replace(@"\n", Environment.NewLine);
 
             using (MessageWriter mw = new MessageWriter())
